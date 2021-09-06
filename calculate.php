9d08af59f3fd29daf4c35479db25e2668d20ab49
Joonas Lume <joonaslume@gmail.com>
2021-09-06 18:47:10 +0300

    Update calculate.php

--- calculate.php
+++ calculate.php
@@ -12,10 +12,10 @@ $context = stream_context_create($opts);
 $json = file_get_contents("https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A?lastNObservations=1&detail=dataonly&dimensionAtObservation=AllDimensions", false, $context);
 
 $exchange_rate = json_decode($json, true)['dataSets'][0]['observations']['0:0:0:0:0:0'][0];
-$car_price = ceil($_GET["car_price"] / $exchange_rate);
+$car_price = ceil($_GET["car_price"] / $exchange_rate) * 1.03;
 $container_price = 1050;
 $customs = ceil($car_price * .1);
-$inner_transport = 650;
+$inner_transport = 850;
 $tax_percentage = .2;
 $documentation_and_service = .15;
 
